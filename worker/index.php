25c25e7ac32993e52ce8b1312116602d0b8fedc7
Alberto <albertord84@gmail.com>
2016-10-26 18:39:00 -0200

    - Include emails:  -> wrong user login email  -> contact form email

--- worker/index.php
+++ worker/index.php
@@ -2,17 +2,26 @@
 
 require_once 'class/Worker.php';
 require_once 'class/system_config.php';
+require_once 'class/Gmail.php';
 
-echo "Worker Inited...!<br>";
+echo "Worker Inited...!<br>\n";
 echo date("Y-m-d h:i:sa");
 
 $GLOBALS['sistem_config'] = new dumbu\cls\system_config();
 
+$Gmail = new dumbu\cls\Gmail();
+$Gmail->send_client_contact_form("Alberto Reyes", "albertord84@gmail.com", "Test contact formm msg", "DUMBU", "555-777-777");
+
+//$Robot = new dumbu\cls\Robot();
+//
+//$Robot->bot_login("josergm86", "joseramon");
+
 $Worker = new dumbu\cls\Worker();
 
-$Worker->delete_daily_work();
-$Worker->prepare_daily_work();
-$Worker->do_work();
+//$Worker->check_daily_work();
+//$Worker->delete_daily_work();
+//$Worker->prepare_daily_work();
+//$Worker->do_work();
 
-echo "<br>" . date("Y-m-d h:i:sa");
+echo "\n<br>" . date("Y-m-d h:i:sa") . "\n\n";
 ?>
