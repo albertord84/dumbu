01bc47de49c13ed34121dedae8b19d2ca0462987
Alberto <albertord84@gmail.com>
2016-11-10 18:16:12 -0200

    Version 0.1

--- worker/class/system_config.php
+++ worker/class/system_config.php
@@ -5,13 +5,13 @@ namespace dumbu\cls {
     class system_config {
 
         const DIALY_REQUESTS_BY_CLIENT = 480; // Change to 480 in production mode
-        const REQUESTS_AT_SAME_TIME = 5;     // Reference Profile Followers amoun by request. Change to 10  in production mode   
+        const REQUESTS_AT_SAME_TIME = 10;     // Reference Profile Followers amoun by request. Change to 10  in production mode   
         const DELAY_BETWEEN_REQUESTS = 0;
-        const MIN_NEXT_ATTEND_TIME = 3; // 5 min --->>> not //5 * 1000 * 60; 
+        const MIN_NEXT_ATTEND_TIME = 5; // 5 min --->>> not //5 * 1000 * 60; 
         const REFERENCE_PROFILE_AMOUNT = 3; // By Client
         const UNFOLLOW_ELAPSED_TIME_LIMIT = 48; // 48; // In hours
         const MAX_GET_FOLLOWERS_REQUESTS = 3; // Max of get followers request to complete REQUESTS_AT_SAME_TIME for a client work
-        const MAX_CLIENT_FAUTL_TRIES = 1; // Quantity max of failures with this client
+        const MAX_CLIENT_FAUTL_TRIES = 2; // Quantity max of failures with this client
         CONST MIN_MARGIN_TO_INIT = 1000;  //margen inicial requerido para trabajar con un cliente        
         
         CONST SYSTEM_EMAIL = 'dumbu.system@gmail.com';
