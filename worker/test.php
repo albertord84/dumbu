6402a12af07b122097832d11950bb6b68b2d6c2d
Alberto <albertord84@gmail.com>
2016-11-25 19:37:59 -0200

    Black Friday Production

--- worker/test.php
+++ worker/test.php
@@ -14,15 +14,23 @@ $GLOBALS['sistem_config'] = new dumbu\cls\system_config();
 
 
 // MUNDIPAGG
-//$Payment = new dumbu\cls\Payment();
-//
-//$Payment->delete_payment("5f4ef87d-cf0d-4da1-91f6-5a394924c308");
+$Payment = new dumbu\cls\Payment();
+
+$payment_data['credit_card_number'] = '5133680051852331';
+$payment_data['credit_card_name'] = 'THIAGO A L BARBALHO';
+$payment_data['credit_card_exp_month'] = '09';
+$payment_data['credit_card_exp_year'] = '2021';
+$payment_data['credit_card_cvc'] = '062';
+$payment_data['amount_in_cents'] = 9990;
+$payment_data['pay_day'] = '1482693376';
+        
+$Payment->create_recurrency_payment($payment_data);
 
 //----------------------------------------------------------------
 
 // GMAIL
 // 
-$Gmail = new dumbu\cls\Gmail();
+//$Gmail = new dumbu\cls\Gmail();
 //$result = $Gmail->send_client_contact_form("Alberto Reyes", "albertord84@gmail.com", "Test contact formm msg NEW2!", "DUMBU", "555-777-777");
 //$Gmail->send_client_payment_error("albertord84@gmail.com", "albertord", "alberto", "Alberto Reyes");
 //$Gmail->send_client_login_error("josergm86@gmail.com", "albertord", "alberto", "Alberto Reyes");
