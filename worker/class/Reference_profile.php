8679ca40ee06f28c1655da8e311492250f4294d5
Alberto <albertord84@gmail.com>
2016-12-06 05:22:35 -0200

    Robot client errors, 5 RP

--- worker/class/Reference_profile.php
+++ worker/class/Reference_profile.php
@@ -2,8 +2,8 @@
 
 namespace dumbu\cls {
     require_once 'Profile.php';
-    //require_once 'DB.php';
-    require_once $_SERVER['DOCUMENT_ROOT'] . '/dumbu/worker/class/DB.php';
+    require_once 'DB.php';
+//    require_once $_SERVER['DOCUMENT_ROOT'] . '/dumbu/worker/class/DB.php';
     //require_once 'Robot.php';    
     /**
      * class Reference_profile
