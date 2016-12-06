0423a8e9c0c4c26d0e7eba766ad020561d4923d0
root <root@vps0417.publiccloud.com.br>
2016-12-06 03:16:51 -0200

    prod on server

--- worker/class/Robot.php
+++ worker/class/Robot.php
@@ -288,7 +288,7 @@ namespace dumbu\cls {
                 //print_r($output);
                 //print("-> $status<br><br>");
                 $json = json_decode($output[0]);
-//                var_dump($output);
+                var_dump($output);
                 if (isset($json->followed_by) && isset($json->followed_by->page_info)) {
                     $DB = new \dumbu\cls\DB();
                     $DB->update_reference_cursor($this->daily_work->reference_id, $json->followed_by->page_info->end_cursor);
