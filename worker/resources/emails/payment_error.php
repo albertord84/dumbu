0d837045294e6cb45d4bd7dd35a87aedf59e2c27
Alberto <albertord84@gmail.com>
2017-01-02 20:00:42 -0200

    wait worker error

--- worker/resources/emails/payment_error.php
+++ worker/resources/emails/payment_error.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <title>DUMBU Payment Problem</title>
+        <title>DUMBU Payment Problem '$diff_days' day(s)!</title>
     </head>
     <body>
         <div style="width: 640px; font-family: Arial, Helvetica, sans-serif; font-size: 11px;">
