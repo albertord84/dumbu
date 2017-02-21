113a80dc7a09e22989c0cc12c6a5dbfdf61aaa09
Jose Remon <josergm86@gmail.com>
2017-02-21 07:40:34 -0300

    retargeting and graphics

--- src/application/controllers/Welcome.php
+++ src/application/controllers/Welcome.php
@@ -70,7 +70,7 @@ class Welcome extends CI_Controller {
     public function get_daily_report($id) {
         if ($this->session->userdata('id')) {
             $this->load->model('class/user_model');
-            $sql = "SELECT * FROM daily_report WHERE client_id=" . $id . " ORDER BY date DESC LIMIT 30;";
+            $sql = "SELECT * FROM daily_report WHERE client_id=" . $id . " ORDER BY date ASC;";  // LIMIT 30
             $result = $this->user_model->execute_sql_query($sql);
             $followings = array();
             $followers = array();
