0cf75bcb5dc08f6a6b82039e8f2424842795cb93
Jose Remon <josergm86@gmail.com>
2017-05-09 17:05:08 -0300

    Delete Auto Like and put meta tag title

--- src/application/views/user_view.php
+++ src/application/views/user_view.php
@@ -8,7 +8,7 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-                <meta name="tile" content="Get Followers on Instagram | Gain or Buy Real & Active Instagram Followers">
+                <meta name="tile" content="<?php echo $CI->T("Ganhar seguidores no Instagram | Ganhar ou Comprar Seguidores Reais e Ativos no Instagram", array()); ?>">
                 <meta name="description" content="<?php echo $CI->T("Ganhar seguidores no Instagram. Aumente seus seguidores reais e qualificados de forma segmentada no Instagram. Followers, curtidas, geolocalizção, direct",array());?>">
                 <meta name="keywords" content="<?php echo $CI->T("ganhar, seguidores, Instagram, seguidores segmentados, curtidas, followers, geolocalizção, direct, vendas",array());?>">
                 <meta name="revisit-after" content="7 days">
