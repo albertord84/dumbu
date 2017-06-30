ebeb301debe0dc6d410b58abd804e519ac9cc03d
Alberto <albertord84@gmail.com>
2017-06-30 19:22:08 -0300

    Check work by reference id

--- src/application/controllers/Worker.php
+++ src/application/controllers/Worker.php
@@ -7,14 +7,17 @@ class Worker extends CI_Controller {
     public function insert_external_work() {
         
     }
-    
-    public function get_work() {
-//        require_once $_SERVER['DOCUMENT_ROOT'] . '/dumbu/worker/class/system_config.php';
-//        $GLOBALS['sistem_config'] = new dumbu\cls\system_config();
-//        require_once $_SERVER['DOCUMENT_ROOT'] . '/dumbu/worker/class/Worker.php';
-//        $Worker = new \dumbu\cls\Worker();
+
+    public function do_work_by_reference_id() {
+        require_once $_SERVER['DOCUMENT_ROOT'] . '/dumbu/worker/class/system_config.php';
+        $GLOBALS['sistem_config'] = new dumbu\cls\system_config();
+        require_once $_SERVER['DOCUMENT_ROOT'] . '/dumbu/worker/class/Worker.php';
+        $Worker = new \dumbu\cls\Worker();
 //        $daily_work = $Worker->get_work();
+        $reference_id = $_GET['reference_id'];
+        $daily_work = $Worker->get_work_by_id($reference_id);
+        $Worker->do_follow_unfollow_work($daily_work);
 //        var_dump($daily_work);
     }
-    
+
 }
