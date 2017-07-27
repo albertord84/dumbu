6bbcb845dbb3d5e81b501816eb98b6adfaeb2212
josergm86 <josergm86@gmail.com>
2017-07-26 21:04:04 -0300

    new meta-tag description

--- src/application/views/purchase_view.php
+++ src/application/views/purchase_view.php
@@ -6,7 +6,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
                 <meta name="title" content="<?php echo $CI->T("Ganhar seguidores no Instagram | Ganhar ou Comprar Seguidores Reais e Ativos no Instagram", array()); ?>">
-                <meta name="description" content="<?php echo $CI->T("Obter seguidores no Instagram. www.dumbu.pro te permite adicionar seguidores de Instagram 100% reais e ativos. Ganhe mais seguidores em Instagram a precios mais baratos!",array());?>">
+                <meta name="description" content="<?php echo $CI->T("Ganhe seguidores no Instagram. www.dumbu.pro te permite ganhar seguidores no Instagram 100% reais e qualificados. Ganhe mais seguidores.",array());?>">
                 <meta name="keywords" content="<?php echo $CI->T("ganhar, seguidores, Instagram, seguidores segmentados, curtidas, followers, geolocalizção, direct, vendas",array());?>">
                 <meta name="revisit-after" content="7 days">
                 <meta name="robots" content="index,follow">
