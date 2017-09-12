36c9d5dccec6d60dd5cd7cb6d355237692d64df5
josergm86 <josergm86@gmail.com>
2017-09-12 07:35:37 -0300

    code for buy

--- worker/scripts/test.php
+++ worker/scripts/test.php
@@ -23,7 +23,7 @@ $GLOBALS['sistem_config'] = new dumbu\cls\system_config();
 //DEBIT CIELO
 $PaymentCielo = new \dumbu\cls\PaymentCielo();
 $payment_data = NULL;
-$PaymentCielo->create_payment_debit($payment_data);
+//$PaymentCielo->create_payment_debit($payment_data);
 
 
 //print $GLOBALS['sistem_config']->SYSTEM_EMAIL . "<br>";
@@ -138,19 +138,19 @@ $Payment = new \dumbu\cls\Payment();
 //$pay_day = strtotime('08/10/2017 00:42:27');
 //$pay_day = strtotime("+30 days", $pay_day);
 
-//$pay_day = time();
+$pay_day = time();
 //$strdate = date("d-m-Y", $pay_day);
 //$pay_day = strtotime("+1 days", time());
 
-//$payment_data['credit_card_number'] = '4984422429563487';
-//$payment_data['credit_card_name'] = 'JOSE MICHEL DE LIMA MACHADO';
-//$payment_data['credit_card_exp_month'] = '03';
-//$payment_data['credit_card_exp_year'] = '2021';
-//$payment_data['credit_card_cvc'] = '186';
-//$payment_data['amount_in_cents'] = 9990;
-//$payment_data['pay_day'] = $pay_day;
-//$resul = $Payment->create_payment($payment_data);
-//var_dump($resul);
+$payment_data['credit_card_number'] = '4415241617725370';
+$payment_data['credit_card_name'] = 'JOSE R G MONTERO';
+$payment_data['credit_card_exp_month'] = '01';
+$payment_data['credit_card_exp_year'] = '2023';
+$payment_data['credit_card_cvc'] = '261';
+$payment_data['amount_in_cents'] = 500;
+$payment_data['pay_day'] = $pay_day;
+$resul = $Payment->create_payment($payment_data);
+var_dump($resul);
 //$resul = $Payment->create_recurrency_payment($payment_data, 0, 32);
 //var_dump($resul);
 //var_dump($pay_day);
