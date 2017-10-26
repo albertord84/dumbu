2df4f2302efa68f95451eefe7eb6f4469cfcd6e1
ruslangm88 <ruslan.guerra88@gmail.com>
2017-10-26 02:15:57 -0200

    adding the Zendesk Chat Script

--- src/application/views/user_view.php
+++ src/application/views/user_view.php
@@ -50,6 +50,17 @@
                 <?php include_once("pixel_facebook.php")?>
                 
                 <script src='https://www.google.com/recaptcha/api.js'></script>
+                
+                <!--Start of Zendesk Chat Script-->
+                <script type="text/javascript">
+                window.$zopim||(function(d,s){var z=$zopim=function(c){
+                z._.push(c)},$=z.s=
+                d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+                _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
+                $.src='https://v2.zopim.com/?4QMQc3y0X75kW162SosdvI6XoWRNlUzo';z.t=+new Date;$.
+                type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
+                </script>
+                <!--End of Zendesk Chat Script-->
 	</head>
 	<body id="my_body">
                 <?php include_once("analyticstracking.php"); ?>            
