2c5b018df0a02f9ed5a9eda36724fb22ec428d80
ruslangm88 <ruslan.guerra88@gmail.com>
2017-10-30 02:01:35 -0200

    update zendesk chat

--- src/application/views/user_view.php
+++ src/application/views/user_view.php
@@ -52,14 +52,32 @@
                 <script src='https://www.google.com/recaptcha/api.js'></script>
                 
                 <!--Start of Zendesk Chat Script-->
-                <script type="text/javascript">
-                window.$zopim||(function(d,s){var z=$zopim=function(c){
-                z._.push(c)},$=z.s=
-                d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-                _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-                $.src='https://v2.zopim.com/?4QMQc3y0X75kW162SosdvI6XoWRNlUzo';z.t=+new Date;$.
-                type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-                </script>
+                <?php
+                    if ($SERVER_NAME == "PRO") {
+                        echo '<script type="text/javascript">
+                        window.$zopim||(function(d,s){var z=$zopim=function(c){
+                        z._.push(c)},$=z.s=
+                        d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+                        _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+                        $.src="https://v2.zopim.com/?4QMQc3y0X75kW162SosdvI6XoWRNlUzo";z.t=+new Date;$.
+                        type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+
+                        $zopim(function() {
+                            $zopim.livechat.departments.filter("");
+                            $zopim.livechat.departments.setVisitorDepartment("Gerente de contas");
+                            $zopim.livechat.setOnConnected(function() {
+                                var dep = $zopim.livechat.departments.getDepartment("Gerente de contas");
+                                if(dep.status=="offline"){
+                                    $zopim.livechat.setStatus("offline");
+                                }
+                                else{
+                                    $zopim.livechat.button.show();
+                                }
+                            })
+                        });
+                        </script>';
+                    }
+                ?>
                 <!--End of Zendesk Chat Script-->
 	</head>
 	<body id="my_body">
