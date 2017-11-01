1f583b5b88c87598bf3a456b55bfdd3fb3be22df
josergm86 <josergm86@gmail.com>
2017-11-01 20:58:15 -0200

    merda q fiz

--- src/application/views/user_view.php
+++ src/application/views/user_view.php
@@ -146,6 +146,21 @@
 								</li>
 							</ul>
 						</li>
+                                               <!-- <li>
+							<a href="#"><?//php echo $CI->T("IDIOMA", array(),$language);?></a>
+							<ul class="dl-submenu">
+								<li>
+                                                                    <div id="login_container1">
+                                                                        <nav class="navbar navbar-default navbar-static-top">
+                                                                           <?php
+                                                                           
+                                                                           ?> 
+                                                                        </nav>
+                                                                    </div>
+								</li>
+							</ul>
+						</li>-->
+                                               
                                                 
 					</ul>
 				</div><!-- /dl-menuwrapper -->
@@ -396,8 +411,7 @@
 			<div class="container">				
                             <div class="col-md-3 col-sm-3 col-xs-12"></div>
                                 <div class="col-md-12 col-sm-12 col-xs-12 text-center">
-                                    <div class="embed-responsive embed-responsive-16by9">
-                                        <iframe class="embed-responsive-item"
+                                    
                                     <?php
                                             if($language=='PT')
                                                 echo '<iframe class="embed-responsive-item" src="https://www.powtoon.com/embed/gtk29HlORyG/" frameborder="0" width="640px" height="360px" allowfullscreen></iframe>';
@@ -405,8 +419,7 @@
                                                 echo '<iframe  class="embed-responsive-item" src="https://www.powtoon.com/embed/bc9vXx9Uxv3/" frameborder="0" width="640px" height="360px" allowfullscreen></iframe>'; 
                                             else echo '<iframe  class="embed-responsive-item" width="854" height="480" src="https://www.youtube.com/embed/9hwWI7eKjVk?ecver=1" frameborder="0" allowfullscreen></iframe>';
                                         ?>
-                                        </iframe>
-                                    </div>
+                                        
                                 </div>
 				<div class="col-md-3 col-sm-3 col-xs-12 text-center"></div>
 			</div>
