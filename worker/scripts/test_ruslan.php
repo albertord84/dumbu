153fa19adfb1c4002518bcbfedbac17500b33908
ruslangm88 <ruslan.guerra88@gmail.com>
2017-12-14 09:58:28 -0200

    test_ruslan.php

--- worker/scripts/test_ruslan.php
+++ worker/scripts/test_ruslan.php
@@ -26,24 +26,24 @@ $GLOBALS['sistem_config'] = new dumbu\cls\system_config();
 // MUNDIPAGG
 $Payment = new \dumbu\cls\Payment();
 
-$pay_day = strtotime('12/25/2017 05:00:00');
+//$pay_day = strtotime('12/25/2017 05:00:00');
 //$pay_day = strtotime("+30 days", $pay_day);
 
-//$pay_day = time();
+$pay_day = time();
 //$strdate = date("d-m-Y", $pay_day);
 //$pay_day = strtotime("+30 days", time());
 
-$payment_data['credit_card_number'] = '5399832655978884';
-$payment_data['credit_card_name'] = 'OLUBUNMI LADENIKA';
-$payment_data['credit_card_exp_month'] = '07';
-$payment_data['credit_card_exp_year'] = '2019';
-$payment_data['credit_card_cvc'] = '856';
-$payment_data['amount_in_cents'] = 24000;
+$payment_data['credit_card_number'] = '5276600091119134';
+$payment_data['credit_card_name'] = 'JULIO CESAR RIBEIRO';
+$payment_data['credit_card_exp_month'] = '12';
+$payment_data['credit_card_exp_year'] = '2018';
+$payment_data['credit_card_cvc'] = '891';
+$payment_data['amount_in_cents'] = 2990;
 $payment_data['pay_day'] = $pay_day;
-//$resul = $Payment->create_payment($payment_data);
-//var_dump($resul);
-$resul = $Payment->create_recurrency_payment($payment_data, 0, 20);
+$resul = $Payment->create_payment($payment_data);
 var_dump($resul);
+//$resul = $Payment->create_recurrency_payment($payment_data, 0, 20);
+//var_dump($resul);
 //$resul = $Payment->create_recurrency_payment($payment_data, 0, 42);
 //var_dump($resul);
 
