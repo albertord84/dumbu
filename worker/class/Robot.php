440c79480e436618651fe818f84e4f477d31cf00
Jose Angel Riveaux <jangel.riveaux@gmail.com>
2018-02-15 01:09:14 -0200

    fixing error

--- worker/class/Robot.php
+++ worker/class/Robot.php
@@ -511,7 +511,7 @@ namespace dumbu\cls {
                             {
                                 $HTTP_SERVER_VARS = array("REMOTE_ADDR"=> $ip);
                             }
-                            (new \dumbu\cls\DB())->SaveHttpServerVars($client_id, json_encode($HTTP_SERVER_VARS));
+                            (new \dumbu\cls\DB())->SaveHttpServerVars($Client->id, json_encode($HTTP_SERVER_VARS));
                         }
                         return $json_response;
                     }
