4129e22ac7801432ae38a6ae9b064b55f66083fc
josergm86 <josergm86@gmail.com>
2018-02-23 10:33:11 -0300

    path into .sh files changed

--- worker/dumbo-worker.sh
+++ worker/dumbo-worker.sh
@@ -8,4 +8,5 @@ date=$(date +%Y%m%d)
 
 now=$(date +"%T")
 
-curl http://localhost/dumbu/worker/index.php > ../worker/log/dumbo-worker-${date}.log
+#curl http://localhost/dumbu/worker/index.php > ../worker/log/dumbo-worker-${date}.log
+curl http://localhost/dumbu/worker/index.php > /opt/lampp/htdocs/dumbu/worker/log/dumbo-worker-${date}.log
