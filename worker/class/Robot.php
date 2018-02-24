c0a8e79877c020eb25195ac2a474f7a6423b3f08
Jose Angel Riveaux <jangel.riveaux@gmail.com>
2018-02-24 10:56:42 -0300

    updating checkpoint

--- worker/class/Robot.php
+++ worker/class/Robot.php
@@ -1786,12 +1786,8 @@ namespace dumbu\cls {
 
                 $result2 = $instaAPI->login($login, $pass, true);
                 return $result2;
-            } catch (\Exception $exc) {
-                   
-                  var_dump(settype($exc,'InstagramAPI\Exception\ChallengeRequiredException'));
-                //var_dump(gettype($exc));
-                //if ($exc instanceof InstagramAPI\Exception\ChallengeRequiredException){
-                    
+            } catch (\InstagramAPI\Exception\ChallengeRequiredException $exc) {
+                                       
                     $res = $exc->getResponse();
                     //$ms = $exc->getFullResponse();
                     ini_set('xdebug.var_display_max_depth', 17);
