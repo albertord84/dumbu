850cdf4c8410acf4cd42d11c95cf7f145adb9480
Jose Angel Riveaux <jangel.riveaux@gmail.com>
2018-02-24 11:36:55 -0300

    updating checkpoint

--- worker/class/Robot.php
+++ worker/class/Robot.php
@@ -1786,7 +1786,7 @@ namespace dumbu\cls {
 
                 $result2 = $instaAPI->login($login, $pass, true);
                 return $result2;
-            } catch (\InstagramAPI\Exception\InstagramException $exc) {
+            } catch (\Exception $exc) {
                                        
                     $res = $exc->getResponse();
                     //$ms = $exc->getFullResponse();
