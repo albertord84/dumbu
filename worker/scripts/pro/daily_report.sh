76c11ddcd4c73e949073334217f89e2dedce484b
josergm86 <josergm86@gmail.com>
2018-02-27 21:29:17 -0300

    creating one and pro .sh files  2

--- worker/scripts/pro/daily_report.sh
+++ worker/scripts/pro/daily_report.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-
-cd "$parent_path"
-
 date=$(date +%Y%m%d)
 
 now=$(date +"%T")
 
-output="../../dumbu/worker/log/daily_report-${date}.log"
-url="http://localhost/dumbu/worker/scripts/daily_report.php"
-wget -O $output $url
+curl http://localhost/dumbu/worker/scripts/daily_report.php > /opt/lampp/htdocs/dumbu/worker/log/daily_report-${date}.log
--- worker/scripts/pro/dumbo-worker7.sh
+++ worker/scripts/pro/dumbo-worker7.sh
@@ -1,17 +1,7 @@
 #!/bin/sh
 
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-
-cd "$parent_path"
-
 date=$(date +%Y%m%d)
 
 now=$(date +"%T")
 
-output="../../dumbu/worker/log/dumbo-worker7-${date}.log"
-#url="http://dumbu.one/dumbu/worker/scripts/index-do.php?id=1"
-url="http://localhost/dumbu/worker/scripts/index-do.php?id=7"
-
-#curl http://localhost/dumbu/worker/index.php > ../worker/log/dumbo-worker-${date}.log
-wget -O $output $url
-#curl http://localhost/dumbu/worker/scripts/index-do.php?id=1 > /opt/lampp/htdocs/dumbu/log/dumbo-worker1-${date}.log
+curl http://localhost/dumbu/worker/scripts/index-do.php?id=7 > /opt/lampp/htdocs/dumbu/worker/log/dumbo-worker7-${date}.log
