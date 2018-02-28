0e4dbe9f7c869501d38b7845a1e239d1fe1cf0b4
josergm86 <josergm86@gmail.com>
2018-02-27 21:13:44 -0300

    creating one and pro .sh files

--- worker/scripts/pro/dumbo-worker1.sh
+++ worker/scripts/pro/dumbo-worker1.sh
@@ -4,4 +4,4 @@ date=$(date +%Y%m%d)
 
 now=$(date +"%T")
 
-curl http://dumbu.pro/dumbu/worker/scripts/index-do.php?id=1 > /opt/lampp/htdocs/dumbu/worker/log/dumbo-worker1-${date}.log
+curl http://localhost/dumbu/worker/scripts/index-do.php?id=1 > /opt/lampp/htdocs/dumbu/worker/log/dumbo-worker1-${date}.log
