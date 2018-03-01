c7357a76b3577656fe9448e840af6e1c2dbc8a4f
josergm86 <josergm86@gmail.com>
2018-03-01 20:28:48 -0300

    tente depois error treated

--- worker/scripts/pro/dumbo-worker3.sh
+++ worker/scripts/pro/dumbo-worker3.sh
@@ -4,4 +4,4 @@ date=$(date +%Y%m%d)
 
 now=$(date +"%T")
 
-curl http://localhost/dumbu/worker/scripts/index-do.php?id=3 > /opt/lampp/htdocs/dumbu/worker/log/dumbo-worker3-${date}.log
+curl http://localhost/dumbu/worker/scripts/index-do.php?id=3 >> /opt/lampp/htdocs/dumbu/worker/log/dumbo-worker3-${date}.log
--- worker/scripts/pro/unfollow.sh
+++ worker/scripts/pro/unfollow.sh
@@ -4,7 +4,7 @@ date=$(date +%Y%m%d)
 
 now=$(date +"%T")
 
-curl http://localhost/dumbu/worker/scripts/unfollow.php > /opt/lampp/htdocs/dumbu/worker/log/unfollow-${date}.log
+curl http://localhost/dumbu/worker/scripts/unfollow.php >> /opt/lampp/htdocs/dumbu/worker/log/unfollow-${date}.log
 
 
 
