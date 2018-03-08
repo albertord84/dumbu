66e3d3fa69bc07a01168c382f35d5863b03c455c
Jose Angel Riveaux <jangel.riveaux@gmail.com>
2018-03-08 09:40:41 -0300

    2 robots

--- worker/scripts/one/check_payment.sh
+++ worker/scripts/one/check_payment.sh
@@ -4,4 +4,4 @@ date=$(date +%Y%m%d)
 
 now=$(date +"%T")
 
-wget -O /home/dumbuo5/public_html/dumbu/src/logs/check-payment-${date}.log   http://127.0.0.1:30080/dumbu/src/index.php/payment/check_payment
+wget -O /home/dumbuo5/public_html/dumbu/src/logs/check-payment-${date}.log   http://dumbu.one/dumbu/src/index.php/payment/check_payment
